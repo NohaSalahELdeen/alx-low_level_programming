d2d808bd8d7526d8a713c0d51fbd64cd9d38ff1f
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-10 09:31:52 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entr point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int x, y;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+if (x != y && x < y)
+{
+putchar(x + 48);
+putchar(y + 48);
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
