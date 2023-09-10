c46168313f8eb4c38c561b104e5a57b8f968c52a
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-10 06:27:09 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int n, l;
-for(n=0; n<10; n++)
-putchar((n%10)+'0');
-for(l='a'; l<='f'; l++)
+for(n = 0; n < 10; n++)
+putchar((n % 10) + '0');
+for(l = 'a'; l <= 'f'; l++)
 putchar(l);
 putchar('\n');
 return (0);
