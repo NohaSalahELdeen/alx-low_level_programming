5f5849b9999c426cefaaa949a2f5f575af60e07d
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-10 06:25:03 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Description
+* describe the parameter
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n, l;
+for(n=0; n<10; n++)
+putchar((n%10)+'0');
+for(l='a'; l<='f'; l++)
+putchar(l);
+putchar('\n');
+return (0);
+}
