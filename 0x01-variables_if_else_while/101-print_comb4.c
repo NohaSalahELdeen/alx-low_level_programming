3c884c89e27a3f13631712eb2172ed077ede282b
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-10 09:51:46 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-/**                                                                                                                                                  
-* main - Entr point                                                                                                                                  
-* Return: Always 0 (Success)                                                                                                                         
+/**
+* main - Entr point
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-  int x, y ,z;
+int x, y ,z;
 for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
-  for (z = 0; z <= 9; z++)
+for (z = 0; z <= 9; z++)
 {
 if (x != y && x < y && y != z && y < z)
 {
@@ -18,10 +18,10 @@ putchar(x + 48);
 putchar(y + 48);
 putchar(z + 48);
 if (x < 7 || y < 8 || z < 9)
-  {
+{
 putchar(',');
 putchar(' ');
-  }
+}
 }
 }
 }
