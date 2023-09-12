e93915fe1c4383b63f8b0a2664a93d570023b086
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-12 13:15:13 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,14 @@
 */
 int main(void)
 {
-char s[8] = "_putchar";
-for (int i = 0; i <= 8; i++)
-{
-_putchar(s[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
