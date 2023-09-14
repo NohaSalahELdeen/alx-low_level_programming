5e2750d2c39dfa4d36aa9e3dbf58d187e9af92df
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-14 08:31:26 -0700

    debugging task 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,23 +3,25 @@
 #include "main.h"
 /**
 * main -Entry point
-* @n: number for checked
+* @i: number for checked
 * Return: 0 (seccess)
 */
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
+/**
+*srand(time(0));
+*i = rand() - RAND_MAX / 2;
+*/
+if (i > 0)
 {
-printf("%d is positive\n", n);
+printf("%d is positive\n", i);
 }
-if (n == 0)
+if (i == 0)
 {
-printf("%d is zero\n", n);
+printf("%d is zero\n", i);
 }
-if (n < 0)
+if (i < 0)
 {
-printf("%d is negative\n", n);
+printf("%d is negative\n", i);
 }
 }
