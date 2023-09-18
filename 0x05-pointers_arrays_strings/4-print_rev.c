cc201aa0085a1d8b52efc87015d2977f7954bd9b
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-18 10:30:49 -0700

    pointers and arrays task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_rev - function name
+* @s: the string
+* Return: void
+*/
+void print_rev(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+continue;	
+for (i = i - 1; s[i] != '\0'; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
