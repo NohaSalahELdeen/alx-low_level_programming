3ab42324b1d50f6c70d25b5b9c7069adc50c8c43
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-21 10:36:24 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,15 +7,19 @@
 char *rot13(char *s)
 {
 int i, j;
-char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 57 ; j++)
+for (j = 0; a[j] != '\0'; j++)
 {
 if (s[i] == a[j])
+{
 s[i] = b[j];
+break;
 }
 }
+}
+s[i] = '\0';
 return (s);
 }
