66781460d7e9c060a2062d3b341671c519d664d7
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-25 07:44:12 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _memset -a function that fills memory with a constant byte.
+* @s: pointer pointed to n.
+* @b: constant byte.
+* @n: bytes of the memory.
+* Return: pointer s.
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; n > 0; i++)
+{
+	n--;
+	s[i] = b;
+}
+	return (s);
+}
