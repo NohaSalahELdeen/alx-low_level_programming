ba26e8ab9b7a866c7de978b632c872a74e22e55f
NohaSalah <nohasalahforstudy@gmail.com>
2023-09-25 12:22:31 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,9 +12,9 @@ char *p;
 for (i = 0; haystack[i] != '\0'; i++)
 {
 	for (j = 0; needle[j] != '\0'; j++)
-	if (needle[j] != haystack[i])
+	if (haystack[j] == needle[i])
 	{
-	p = &needle[j];
+	p = &haystack[j - i];
 	return (p);
 	}
 }
