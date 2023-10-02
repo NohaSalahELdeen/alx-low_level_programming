108345399adee49201feabe3117a381af460739a
NohaSalah <nohasalahforstudy@gmail.com>
2023-10-02 16:04:45 -0700

    argc and argv task 5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main function.
+* @argc: first main argument.
+* @argv: array.
+* Return: 0 or 1
+*/
+int main(int argc, char *argv[])
+{
+int cent, coins = 0;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+cent = atoi(argv[1]);
+while (cent > 0)
+{
+if (cent >= 25)
+	cent -= 25;
+if (cent >= 10)
+	cent -= 10;
+if (cent >= 5)
+	cent -= 5;
+if (cent >= 2)
+	cent -= 2;
+if (cent >= 1)
+	cent -= 1;
+coins++;
+}
+printf("%d\n", coins);
+return (0);
+}
