8b123007a8d6081adcae991385e83f06a658ca7c
NohaSalah <nohasalahforstudy@gmail.com>
2023-10-02 11:21:37 -0700

    0x0A task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - basic function.
+* @argc: first argument.
+* @argv: second argument
+* Return: always 0.
+*/
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
