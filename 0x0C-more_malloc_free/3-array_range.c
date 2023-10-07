dd44ff5b8904bb00829097fd7208c4910a22e89a
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-07 15:27:29 -0700

    more malloc task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,15 +11,18 @@ int *array_range(int min, int max)
 {
 	int *arr, i;
 
-	if (min > max)
-		return (NULL);
-
 	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == 0)
 		return (NULL);
 
+	if (min > max)
+	{
+		return (NULL);
+	}
+	else
+	{
 	for (i = 0; min <= max; i++)
 		arr[i] = min++;
-
+	}
 	return (arr);
 }
