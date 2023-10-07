acd5863053e248a09e91f6d62c1ed8cefdbe44b0
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-07 13:49:29 -0700

    more malloc task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(char) * nmemb);
+	arr = malloc(size * nmemb);
 	if (arr == NULL)
 		return (NULL);
 
