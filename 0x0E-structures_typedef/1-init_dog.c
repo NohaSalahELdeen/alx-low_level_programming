bf62529a6de2ad323da3f61021d865f91b2c0a0d
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-10 09:59:28 -0700

    structures_typedef task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+  * init_dog -  initialize a variable
+  * @d: pointer to variable name
+  * @name: dog name
+  * @age: dog age
+  * @owner: dog owner
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
