90a33740c5fa843906d19c8328f5b47d581ec198
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-19 13:46:31 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * before_main - print before main executed
+  *
+  */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
