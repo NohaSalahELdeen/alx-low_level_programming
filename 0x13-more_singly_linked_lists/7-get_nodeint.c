a83fef06740e708669e42d6d2ed130d8fa0233f0
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-24 14:13:53 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -17,6 +17,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	for (i = 0; i < index; i++)
+	{
 		ptr = ptr->next;
+		if (ptr == NULL)
+			return (NULL);
+	}
 	return (ptr);
 }
