d89b69afacda47d86d42192192a4437fd263d8ef
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-26 15:51:04 -0700

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * get_endianness - checks the endianness.
+  * Return: 0 if big endian, 1 if little endian.
+  */
+
+int get_endianness(void)
+{
+	int n;
+	char *c = (char *)&n;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
