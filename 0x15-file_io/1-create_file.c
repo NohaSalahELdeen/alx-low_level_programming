c5b5ba1815a2ae3967556fd7579d78cd22c3561d
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-30 15:44:01 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	c_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 500);
+	c_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
 	if (c_file == -1)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
