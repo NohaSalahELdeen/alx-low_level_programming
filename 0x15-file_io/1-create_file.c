0e22eb5f752ee845e8ab23a7884ba55c1cf8a7fa
Noha Salah <nohasalahforstudy@gmail.com>
2023-10-31 10:45:43 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
   * create_file - creates a file.
@@ -9,23 +10,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int c_file, i, len, wr;
+	int c_file, wr;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
 		text_content = "";
 
-	c_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	c_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (c_file == -1)
 		return (-1);
-	for (i = 0; text_content[i] != '\0'; i++)
-		len++;
-	wr = write(c_file, text_content, len);
+
+	wr = write(c_file, text_content, strlen(text_content));
 
 	if (wr == -1)
+	{
+		close(c_file);
 		return (-1);
+	}
 	close(c_file);
-
 	return (1);
 }
