014d0c51dba914e35729f7687eae23b369bbcdd3
Noha Salah <nohasalahforstudy@gmail.com>
2024-03-25 07:05:16 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/calculation.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
